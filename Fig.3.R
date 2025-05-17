23feede0dea78760f638fcca3914847f54251917
Hojun Lee <131523616+freenergy777@users.noreply.github.com>
2025-05-17 17:47:42 +0900

    Update Fig.3.R

--- Fig.3.R
+++ Fig.3.R
@@ -48,7 +48,7 @@ data = data[!grepl("^RP", rownames(data)),] # delete ribosomal gene data
 data = scater::logNormCounts(data)
 data$tissue_major = paste0(data$summaryDescription, "__", 
                            data$broad_type)
-keep = names(which(table(paste0(data$summaryDescription, "__", data$broad_type)) > 100)) # cell type sorting
+keep = names(which(table(paste0(data$summaryDescription, "__", data$broad_type)) > 100)) # cell type sorting 
 data = data[, data$tissue_major %in% keep]
 # this brings down the number of cells from 187200 to 186610
 table(data$broad_type)
@@ -79,7 +79,6 @@ data_seurat <- Seurat::as.Seurat(data_seurat, counts='counts', data = 'logcounts
   FindClusters(random.seed = 123, algorithm = 4) # algorithm 4 for leiden cluster
 saveRDS(data_seurat, file = 'D:/2025.04/monocle3_practice/20250420_3k/20250420_3k_SCTransform_suera.rds')
 
-data_seurat <- readRDS('/home/hjlee/20250420_3k_SCTransform_suerat.rds')
 DimPlot(data_seurat, reduction = "umap", group.by = "broad_type") 
 DimPlot(data_seurat, reduction = "umap", group.by = "summaryDescription")
 
@@ -107,9 +106,73 @@ dfumap_elgn3_negative_RCCs <- dfumap %>%
 dfumap_RCCs$EGLN3_expression <- factor(dfumap_RCCs$EGLN3_expression, levels = c('Negative', 'Positive'))
 dfumap_RCCs_filtered$EGLN3_expression <- factor(dfumap_RCCs_filtered$EGLN3_expression, levels = c('Negative', 'Positive'))
 
-p1 <- ggboxplot(dfumap_RCCs_filtered, x='EGLN3_expression', y='pc_sig', width = 0.4, outlier.shape = 1, size = 0.3,
+p1 <- ggboxplot(dfumap_RCCs_filtered, x='EGLN3_expression', y='pc_sig', width = 0.4, outlier.shape = 1, size = 0.4, outlier.size = 0.5,
                color = 'black', notch = T, xlab = 'EGLN3 expression', ylab = 'PC related protein \n transcriptomic signatuare',
                fill = 'EGLN3_expression') +
   stat_compare_means(method = "t.test", label = "p.signif", comparisons = list(c('Negative', 'Positive')), bracket.size = 0.3) 
 p1 <- p1 + scale_fill_manual(values = c('Negative'='skyblue3', 'Positive'='red1'))
 ggarrange(p1)
+
+# Gene expression
+
+dfqc = do.call(rbind, lapply(keep, function(x) {
+  
+  idx = data$tissue_major == x
+  data_subset = logcounts(data[c(goi, PC_genes), idx])
+  
+  df = data.frame(
+    label = x,
+    gene = rownames(data_subset),
+    mean = rowMeans(data_subset),
+    prop = rowSums(data_subset > 0)/ncol(data_subset)
+  )
+  return(df)
+  
+}))
+
+dfqc$variable = as.character(dfqc$label)
+dfqc$tissue = sapply(strsplit(dfqc$label, "__"), "[[", 1)
+dfqc$major = sapply(strsplit(dfqc$label, "__"), "[[", 2)
+dfqc$type = dfqc$tissue
+dfqc$type[grepl("Normal", dfqc$type)] = "Normal"
+dfqc$type[grepl("Tumour.normal", dfqc$type)] = "Adjacent"
+dfqc$type[grepl("Tumour", dfqc$type)] = "Tumour"
+dfqc$tissue = gsub("Normal.adrenal", "Adrenal", dfqc$tissue)
+dfqc$tissue = gsub("Normal.kidney", "Kidney", dfqc$tissue)
+
+dfqc$type = factor(dfqc$type, levels = c("Normal", "Adjacent","Tumour", "Metastasis"))
+dfqc$type = factor(dfqc$type, levels = c("Normal", "Adjacent","Tumour", "Metastasis"))
+
+dfqc$major = factor(dfqc$major, levels = c("RCC", "Epi_PT", "Epi_non-PT", "EC", "Fibro", "T-cell", "B-cell",
+                                           "Myeloid", "NK", "Plasma", "Mast","pDC"))
+
+p1 = dfqc %>%
+  filter(gene != "EGLN3") %>%
+  ggplot(aes(x = type, y = gene, col = mean, size = prop)) + 
+  geom_point() + scale_color_distiller(palette = "Spectral") + 
+  theme(axis.text.x = element_text(angle = 90)) + facet_wrap(~major, ncol = 12)
+
+p2 = dfqc %>%
+  filter(gene == "EGLN3") %>%
+  ggplot(aes(x = type, y = gene, col = mean, size = prop)) + 
+  geom_point() + scale_color_distiller(palette = "Spectral") + 
+  theme(axis.text.x = element_text(angle = 90)) + facet_wrap(~major, ncol = 12)
+
+p1 %>% ggsave(filename = "markers1.pdf", width = 14, height = 2.8)
+p2 %>% ggsave(filename = "markers2.pdf", width = 14, height = 2)
+
+dfqc[dfqc$gene == "EGLN3" & dfqc$major == "RCC",]
+mean(dfqc[dfqc$gene == "EGLN3" & dfqc$major %in% c("Epi_PT", "Epi_non-PT"),"prop"])
+
+dfqc2 = dfqc %>%
+  group_by(major, gene) %>%
+  summarise(prop = mean(prop))
+
+propviz = dfqc2 %>%
+  filter(gene == "EGLN3") %>%
+  ggplot(aes(reorder(major, prop, max), prop, fill = major)) + 
+  geom_col() + ylab("Proportion of cells expressing EGLN3") + 
+  scale_fill_manual(values = broad_col) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_text(angle = 90))
+
+propviz %>% ggsave(filename = "prop.pdf", width = 5, height = 5)
