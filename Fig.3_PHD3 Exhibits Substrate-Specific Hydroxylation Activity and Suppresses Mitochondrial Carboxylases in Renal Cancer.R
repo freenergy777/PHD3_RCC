dd0116270e55213afa4989016c2d003f26d3611d
Hojun Lee <131523616+freenergy777@users.noreply.github.com>
2025-05-17 17:19:41 +0900

    Fig.3_PHD3 Exhibits Substrate-Specific Hydroxylation Activity and Suppresses Mitochondrial Carboxylases in Renal Cancer.R

---
+++ Fig.3_PHD3 Exhibits Substrate-Specific Hydroxylation Activity and Suppresses Mitochondrial Carboxylases in Renal Cancer.R
@@ -0,0 +1,127 @@
+suppressPackageStartupMessages({
+  library(rhdf5)
+  library(zellkonverter)
+  library(SingleCellExperiment)
+  library(dplyr)
+  library(ggplot2)
+  library(Seurat)
+  library(monocle3)
+  library(tibble)
+  library(RColorBrewer)
+  library(openxlsx)
+  library(SeuratWrappers)
+  library(pheatmap)
+  library(harmony)
+  library(sctransform)
+  library(scater)
+  library(cepo)
+  library(glmGamPoi)
+  library(future)
+  library(reticulate)
+  library(future)
+  library(future.apply)
+})
+
+f = "RCC_upload_final_raw_counts.h5ad"
+h5ls(f)
+data <- readH5AD(file = f,
+                 verbose=T, layers=F, varm=F,
+                 obsm=F, varp=F, obsp=F, uns=F)
+counts(data) = data@assays@data$X
+
+# Major level annotation
+table(data$broad_type)
+# Minor level annotation
+table(data$annotation)
+# Patients
+table(data$patient)
+# Tissue
+table(data$summaryDescription)
+
+data <- data[, !data$summaryDescription %in% c("Blood", "Fat", "Thrombus")]
+data <- data[,data@colData@listData[["percent.mt"]] < 10]
+# this brings down the number of cells from 270855 to 187200
+
+# edit gene expression data
+data@assays@data$X = NULL 
+data = data[!grepl("^RP", rownames(data)),] # delete ribosomal gene data
+data = scater::logNormCounts(data)
+data$tissue_major = paste0(data$summaryDescription, "__", 
+                           data$broad_type)
+keep = names(which(table(paste0(data$summaryDescription, "__", data$broad_type)) > 100)) # cell type sorting
+data = data[, data$tissue_major %in% keep]
+# this brings down the number of cells from 187200 to 186610
+table(data$broad_type)
+
+PC_genes = c("PC","PCCA","PCCB","MCCC1","MCCC2","ACACA","ACACB")
+data$egln3 = logcounts(data)["EGLN3",] # genes are stored in row & cells are stored in column
+data$pc = logcounts(data)["PC",]
+data$pc_sig = colMeans(logcounts(data)[PC_genes,])
+data$coexp = data$egln3 > 0 & data$pc_sig > 0
+
+#################################################################
+# pre-processing & normalization
+data_seurat = do.call(cbind, lapply(unique(data$broad_type), function(x) {
+  subset = data[, data$broad_type == x]
+  if (ncol(subset) > 15000) {
+    set.seed(1)
+    subset = subset[, sample(1:ncol(subset), 15000)]
+  }
+  return(subset)
+}))
+# batch effect normalization, non-linear dimensionality reduction
+plan("multisession", workers=4)
+set.seed(123)
+options(future.seed = TRUE)
+data_seurat <- Seurat::as.Seurat(data_seurat, counts='counts', data = 'logcounts') %>% # count as raw data; data as log transformed data
+  PercentageFeatureSet(pattern = "^MT-", col.name = "percent.mt") %>%
+  SCTransform(assay = "originalexp", vars.to.regress = "percent.mt", verbose = T, method 
+              ='glmGamPoi', n_genes=3000,
+              conserve.memory = TRUE, return.only.var.genes = TRUE) %>%
+  RunPCA(npcs=10, approx = TRUE) %>%
+  RunHarmony(group.by.vars = "patient", assay.use = "SCT") %>%
+  FindNeighbors(dims = 1:10, reduction = 'harmony') %>% # for SNN
+  RunUMAP(dims = 1:10, umap.method = "uwot", reduction = 'harmony') %>%
+  FindClusters(random.seed = 123, algorithm = 4) # algorithm 4 for leiden cluster
+saveRDS(data_seurat, file = 'D:/2025.04/monocle3_practice/20250420_3k/20250420_3k_SCTransform_suera.rds')
+
+data_seurat <- readRDS('/home/hjlee/20250420_3k_SCTransform_suerat.rds')
+p <- DimPlot(data_seurat, reduction = "umap", group.by = "broad_type") 
+print(p)
+p <- DimPlot(data_seurat, reduction = "umap", group.by = "broad_type") +
+  geom_segment(aes(x = -10, xend = 10, y = -10, yend = -10), inherit.aes = FALSE, size = 0.8, color = 'black') +
+  geom_segment(aes(x = -10, xend = -10, y = -10, yend = 10), inherit.aes = FALSE, size = 0.8, color = 'black') +
+  coord_cartesian(xlim = c(-10, 10), ylim = c(-10, 10))
+
+DimPlot(data_seurat, reduction = "umap", group.by = "summaryDescription")
+
+#################################################################
+rd = Embeddings(data_seurat, "umap")
+dfumap = as.data.frame(rd)
+dfumap$tissue = data_seurat$summaryDescription
+dfumap$tissue_major = data_seurat$tissue_major
+dfumap$broad = data_seurat$broad_type
+dfumap$egln3 = data_seurat@assays$originalexp@data["EGLN3",]
+dfumap$EGLN3_expression = ifelse(dfumap$egln3>0, 'Positive', 'Negative') 
+dfumap$pc_sig = colMeans(data_seurat@assays$originalexp@data[PC_genes,])
+dfumap$pc = data_seurat@assays$originalexp@data["PC",]
+dfumap$coexp = dfumap$egln3 > 0 & dfumap$pc_sig > 0
+
+dfumap_RCCs <- dfumap %>%
+  filter(tissue=='Tumour'& tissue_major=='Tumour__RCC'& broad=='RCC')
+dfumap_RCCs_filtered <- dfumap %>%
+  filter(tissue=='Tumour'& tissue_major=='Tumour__RCC'& broad=='RCC'& pc_sig>0)
+dfumap_elgn3_positive_RCCs <- dfumap %>%
+  filter(tissue=='Tumour'& tissue_major=='Tumour__RCC'& broad=='RCC'& EGLN3_expression=='Positive')
+dfumap_elgn3_negative_RCCs <- dfumap %>%
+  filter(tissue=='Tumour'& tissue_major=='Tumour__RCC'& broad=='RCC'& EGLN3_expression=='Negative')
+
+dfumap_RCCs$EGLN3_expression <- factor(dfumap_RCCs$EGLN3_expression, levels = c('Negative', 'Positive'))
+dfumap_RCCs_filtered$EGLN3_expression <- factor(dfumap_RCCs_filtered$EGLN3_expression, levels = c('Negative', 'Positive'))
+
+p1 <- ggboxplot(dfumap_RCCs_filtered, x='EGLN3_expression', y='pc_sig', width = 0.4, outlier.shape = 1, size = 0.3,
+               color = 'black', notch = T, xlab = 'EGLN3 expression', ylab = 'PC related protein \n transcriptomic signatuare',
+               fill = 'EGLN3_expression') +
+  stat_compare_means(method = "t.test", label = "p.signif", comparisons = list(c('Negative', 'Positive')), bracket.size = 0.3) 
+p1 <- p1 + scale_fill_manual(values = c('Negative'='skyblue3', 'Positive'='red1'))
+ggarrange(p1)
