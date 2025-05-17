a0a62b2211affa68645d19f9b0dfbb04437a4743
Hojun Lee <131523616+freenergy777@users.noreply.github.com>
2025-05-17 17:47:45 +0900

    Update Fig.6.R

--- Fig.6.R
+++ Fig.6.R
@@ -58,3 +58,51 @@ data$egln3 = logcounts(data)["EGLN3",] # genes are stored in row & cells are sto
 data$pc = logcounts(data)["PC",]
 data$pc_sig = colMeans(logcounts(data)[PC_genes,])
 data$coexp = data$egln3 > 0 & data$pc_sig > 0
+
+#################################################################
+# pre-processing & normalization
+data_seurat = do.call(cbind, lapply(unique(data$broad_type), function(x) {
+  subset = data[, data$broad_type == x]
+  if (ncol(subset) > 15000) {
+    set.seed(1)
+    subset = subset[, sample(1:ncol(subset), 15000)]
+  }
+  return(subset)
+}))
+# batch effect normalization, non-linear dimensionality reduction
+plan("multisession", workers=4)
+set.seed(123)
+options(future.seed = TRUE)
+data_seurat <- Seurat::as.Seurat(data_seurat, counts='counts', data = 'logcounts') %>% # count as raw data; data as log transformed data
+  PercentageFeatureSet(pattern = "^MT-", col.name = "percent.mt") %>%
+  SCTransform(assay = "originalexp", vars.to.regress = "percent.mt", verbose = T, method 
+              ='glmGamPoi', n_genes=3000,
+              conserve.memory = TRUE, return.only.var.genes = TRUE) %>%
+  RunPCA(npcs=10, approx = TRUE) %>%
+  RunHarmony(group.by.vars = "patient", assay.use = "SCT") %>%
+  FindNeighbors(dims = 1:10, reduction = 'harmony') %>% # for SNN
+  RunUMAP(dims = 1:10, umap.method = "uwot", reduction = 'harmony') %>%
+  FindClusters(random.seed = 123, algorithm = 4) # algorithm 4 for leiden cluster
+saveRDS(data_seurat, file = 'D:/2025.04/monocle3_practice/20250420_3k/20250420_3k_SCTransform_suera.rds')
+
+DimPlot(data_seurat, reduction = "umap", group.by = "broad_type") 
+DimPlot(data_seurat, reduction = "umap", group.by = "summaryDescription")
+
+#################################################################
+p1 = ggplot(dfumap, aes(umap_1, umap_2, col = broad)) + 
+  ggrastr::geom_point_rast(alpha = 1, size = 0.005) + 
+  scale_color_manual(values = broad_col) + 
+  theme_classic() + ggtitle("Cell type annotations") + 
+  theme(aspect.ratio = 1)
+
+p2 = ggplot(dfumap, aes(umap_1, umap_2, col = egln3)) + 
+  ggrastr::geom_point_rast(alpha = 1, size = 0.005) + 
+  scale_color_viridis_c(option = "C") + 
+  theme_classic() + ggtitle("EGLN3 expression") + 
+  theme(aspect.ratio = 1)
+
+tiff(filename = "umap_celltype.tiff", units= 'cm', width = 30, height = 20, res = 600)
+tiff(filename = "umap_egln3.tiff", units= 'cm', width = 30, height = 20, res = 600)
+print(p1)
+dev.off()
+
